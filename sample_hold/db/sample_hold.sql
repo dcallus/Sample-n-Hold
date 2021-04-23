cf2d3329129d918779a2e0d342226b50a0733165
Daniel Callus <danielcallus@gmail.com>
2021-04-23 18:20:26 +0100

    created empty tables in database

---
+++ sample_hold/db/sample_hold.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS modules;
+DROP TABLE IF EXISTS manufacturers;
+
+CREATE TABLE manufacturers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),
+  phone VARCHAR(255),
+  website VARCHAR(255)
+);
+
+CREATE TABLE modules (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  stock INT,
+  buying_cost INT,
+  selling_price INT,
+  function VARCHAR(255),
+  width INT,
+  depth INT,
+  image_url VARCHAR(255),
+  minus_12v INT,
+  plus_12v INT,
+  manufacturer_id INT REFERENCES manufacturers(id)
+);
