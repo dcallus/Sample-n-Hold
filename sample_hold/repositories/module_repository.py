5ffe3e7da2fa81c2a26d39d55ea79f6aea461b0d
Daniel Callus <danielcallus@gmail.com>
2021-04-24 07:50:46 +0100

    added package inits

---
+++ sample_hold/repositories/module_repository.py
@@ -0,0 +1,58 @@
+from db.run_sql import run_sql
+
+from models.book import Book
+from models.author import Author
+import repositories.author_repository as author_repository
+
+
+def save(book):
+    sql = "INSERT INTO books (title, genre, publisher, author_id) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [book.title, book.genre, book.publisher, book.author.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    book.id = id
+    return book
+
+
+def select_all():
+    books = []
+
+    sql = "SELECT * FROM books"
+    results = run_sql(sql)
+
+    for row in results:
+        author = author_repository.select(row['author_id'])
+        book = Book(row['title'], row['genre'], row['publisher'], author, row['id'] )
+        books.append(book)
+    return books
+
+
+
+def select(id):
+    book = None
+    sql = "SELECT * FROM books WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        author = author_repository.select(result['author_id'])
+        book = Book(result['title'], result['genre'], result['publisher'], author, result['id'] )
+    return book
+
+
+def delete_all():
+    sql = "DELETE  FROM books"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE  FROM books WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update(book):
+    sql = "UPDATE books SET (title, genre, publisher, author_id) = (%s, %s, %s, %s) WHERE id = %s"
+    values = [book.title, book.genre, book.publisher, book.author.id, book.id]
+    print(values)
+    run_sql(sql, values)
