6ba756a6c37dd28637f8f860df125448f348e26a
Daniel Callus <danielcallus@gmail.com>
2021-04-24 13:03:36 +0100

    create module flask done

---
+++ sample_hold/controllers/modules_controller.py
@@ -0,0 +1,77 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.module import Module
+import repositories.module_repository as module_repository
+import repositories.manufacturer_repository as manufacturer_repository
+
+modules_blueprint = Modules("modules", __name__)
+
+@modules_blueprint.route("/modules")
+def modules():
+    modules = module_repository.select_all() # NEW
+    return render_template("modules/index.html", all_modules = modules)
+
+# NEW
+# GET '/modules/new'
+@modules_blueprint.route("/modules/new", methods=['GET'])
+def new_module():
+    manufacturers = manufacturer_repository.select_all()
+    return render_template("modules/new.html", all_manufacturers = manufacturers)
+
+# CREATE
+# POST '/modules'
+@modules_blueprint.route("/modules",  methods=['POST'])
+def create_module():
+    name = request.form['name']
+    description = request.form['description']
+    stock = request.form['stock']
+    buying_cost = request.form['buying-cost']
+    selling_price = request.form['selling-price']
+    function = request.form['function']
+    width = request.form['width']
+    depth = request.form['depth']
+    minus_12v = request.form['minus-12v']
+    plus_12v = request.form['plus-12v']
+    manufacturer  = manufacturer_repository.select(request.form['manufacturer_id'])
+    image_url = request.form['image-url']
+    module = Module(row['name'], row['description'], row['stock'], row['buying_cost'], row['selling_price'],
+                    row['function'], row['width'], row['depth'], row['minus_12v'], row['plus_12v'], row['manufacturer_id'], 
+                    row['image_url'], row['id'])
+    module_repository.save(module)
+    return redirect('/modules')
+
+
+# # SHOW
+# # GET '/modules/<id>'
+# @modules_blueprint.route("/modules/<id>", methods=['GET'])
+# def show_module(id):
+#     module = module_repository.select(id)
+#     return render_template('modules/show.html', module = module)
+
+# # EDIT
+# # GET '/modules/<id>/edit'
+# @modules_blueprint.route("/modules/<id>/edit", methods=['GET'])
+# def edit_module(id):
+#     module = module_repository.select(id)
+#     manufacturers = manufacturer_repository.select_all()
+#     return render_template('modules/edit.html', module = module, all_manufacturers = manufacturers)
+
+# # UPDATE
+# # PUT '/modules/<id>'
+# @modules_blueprint.route("/modules/<id>", methods=['POST'])
+# def update_module(id):
+#     title    = request.form['title']
+#     genre = request.form['genre']
+#     publisher   = request.form['publisher']
+#     manufacturer  = manufacturer_repository.select(request.form['manufacturer_id'])
+#     module = module(title, genre, publisher, manufacturer, id)
+#     print(module.manufacturer.full_name())
+#     module_repository.update(module)
+#     return redirect('/modules')
+
+# # DELETE
+# # DELETE '/modules/<id>'
+# @modules_blueprint.route("/modules/<id>/delete", methods=['POST'])
+# def delete_module(id):
+#     module_repository.delete(id)
+#     return redirect('/modules')
