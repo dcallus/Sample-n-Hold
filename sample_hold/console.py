48d7efdca3e0b757001de8421439f92f26ab2d6d
Daniel Callus <danielcallus@gmail.com>
2021-04-24 17:17:43 +0100

    fixed margin

--- sample_hold/console.py
+++ sample_hold/console.py
@@ -24,7 +24,18 @@ It's really two modules in one: a totally unique analogue bassdrum and a voltage
 - voltage control of pitch (approximates v/oct response, but not musically tuned)
 - non-invasive analogue optocouplers used for voltage control, thus, completely vintage sounds can still be had if that's what you're after!
 - completely redesigned, hi-fi signal chain spares no expense by replacing cheap transistor amplifiers with low noise opamp equivalents
+- you can dial in super hot, modular level output levels (up to 20Vp-p!)
+
+- analogue bassdrum circuitry capable of vanilla TR-808 sounds
+- built in voltage controlled distortion (batteryACID distortion)
+- distortion accepts external inputs when not being used with kick drum (great for acid synth leads)
+- voltage control of distortion amount
+- voltage control of decay from short kicks to long "ooooooooomphs"
+- voltage control of pitch (approximates v/oct response, but not musically tuned)
+- non-invasive analogue optocouplers used for voltage control, thus, completely vintage sounds can still be had if that's what you're after!
+- completely redesigned, hi-fi signal chain spares no expense by replacing cheap transistor amplifiers with low noise opamp equivalents
 - you can dial in super hot, modular level output levels (up to 20Vp-p!)"""
+
 hex_address = "HEXINVERTER ÉLECTRONIQUE, Somewhere in Canada, Maybe Montreal?"
 
 hexinverter = Manufacturer("Hexinverter", hex_address, "1-555-2345")
