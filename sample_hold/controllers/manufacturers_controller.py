bfc620ecff05566b3ed92d460765dcf170be7108
Daniel Callus <danielcallus@gmail.com>
2021-04-26 14:43:40 +0100

    manufacturer edit complete

--- sample_hold/controllers/manufacturers_controller.py
+++ sample_hold/controllers/manufacturers_controller.py
@@ -36,3 +36,36 @@ def create_manufacturer():
 def show_manufacturer(id):
     manufacturer = manufacturer_repository.select(id)
     return render_template('/manufacturers/show.html', manufacturer = manufacturer)
+
+# EDIT
+# GET '/manufacturers/<id>/edit'
+@manufacturers_blueprint.route("/manufacturers/<id>/edit", methods=['GET'])
+def edit_manufacturer(id):
+    manufacturer = manufacturer_repository.select(id)
+    manufacturers = manufacturer_repository.select_all()
+    return render_template('manufacturers/edit.html', manufacturer = manufacturer)
+
+# UPDATE
+# PUT '/manufacturers/<id>'
+@manufacturers_blueprint.route("/manufacturers/<id>", methods=['POST'])
+def update_manufacturer(id):
+
+    name = request.form['name']
+    description = request.form['description']
+    stock = request.form['stock']
+    buying_cost = request.form['buying-cost']
+    selling_price = request.form['selling-price']
+    function = request.form['function']
+    width = request.form['width']
+    depth = request.form['depth']
+    minus_12v = request.form['minus-12v']
+    plus_12v = request.form['plus-12v']
+    manufacturer_id = request.form["manufacturer-id"]
+    manufacturer  = manufacturer_repository.select(manufacturer_id)
+    image_url = request.form['image-url']
+    manufacturer = Manufacturer(name, description, stock, buying_cost, selling_price,
+                    function, width, depth, minus_12v, plus_12v, manufacturer, 
+                    image_url, id)
+    manufacturer_repository.update(manufacturer)
+
+    return redirect('/manufacturers')
