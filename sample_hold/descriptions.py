3f3fbb502cdc2019b787241296ecbe917696f7c2
Daniel Callus <danielcallus@gmail.com>
2021-04-28 16:48:00 +0100

    fixed image resize issue

---
+++ sample_hold/descriptions.py
@@ -0,0 +1,13 @@
+mutant_bassdrum_description = """====Primary Features====
+It's really two modules in one: a totally unique analogue bassdrum and a voltage controlled distortion module! (with the same topology as batteryACID!)
+
+- analogue bassdrum circuitry capable of vanilla TR-808 sounds
+- built in voltage controlled distortion (batteryACID distortion)
+- distortion accepts external inputs when not being used with kick drum (great for acid synth leads)
+- voltage control of distortion amount
+- voltage control of decay from short kicks to long "ooooooooomphs"
+- voltage control of pitch (approximates v/oct response, but not musically tuned)
+- non-invasive analogue optocouplers used for voltage control, thus, completely vintage sounds can still be had if that's what you're after!
+- completely redesigned, hi-fi signal chain spares no expense by replacing cheap transistor amplifiers with low noise opamp equivalents
+- you can dial in super hot, modular level output levels (up to 20Vp-p!)"""
+
