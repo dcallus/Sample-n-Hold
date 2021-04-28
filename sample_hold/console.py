86a448403b1697088ff7958de40c395d1356f51f
Daniel Callus <danielcallus@gmail.com>
2021-04-28 17:41:21 +0100

    more db stuff added

--- sample_hold/console.py
+++ sample_hold/console.py
@@ -16,34 +16,66 @@ hex_address = "HEXINVERTER ÉLECTRONIQUE, Somewhere in Canada, Maybe Montreal?"
 
 doepfer = Manufacturer("Doepfer", "somewhere in Germany", "555-555-555", "http://www.doepfer.de/")
 hexinverter = Manufacturer("Hexinverter", hex_address, "1-555-2345")
+
+klangbau = Manufacturer("Klangbau Köln", "Berlin, Germany", "555-555-505","https://xn--klangbaukln-zfb.de/")
+hexinverter = Manufacturer("Hexinverter", hex_address, "1-555-2345")
+
 manufacturer_repository.save(doepfer)
-
-
+manufacturer_repository.save(hexinverter)
+manufacturer_repository.save(klangbau)
 manufacturer_repository.save(hexinverter)
 
 
 # MODULES
 
 mutant_bassdrum_url = "https://www.modulargrid.net/img/imagecache/212x413_3535.jpg"
+mutant_clap_url = "https://www.modulargrid.net/img/imagecache/212x413_4062.jpg"
+mutant_machine_url = "https://www.modulargrid.net/img/imagecache/300x262_24839.jpg"
+mutant_hotglue_url = "https://www.modulargrid.net/img/imagecache/300x317_5288.jpg"
 
+twin_peak_res_url = "https://www.modulargrid.net/img/imagecache/235x413_12514.jpg"
 
+makenoise_math_url = "https://www.modulargrid.net/img/imagecache/300x381_20545.jpg"
 
+a_148_url = "https://www.modulargrid.net/img/imagecache/64x413_82.jpg"
+a_160_url = "https://www.modulargrid.net/img/imagecache/65x413_91.jpg"
+a_160_2_url = "https://www.modulargrid.net/img/imagecache/66x413_4946.jpg"
+a_160_5_url = "https://www.modulargrid.net/img/imagecache/64x413_5278.jpg"
 
+pittsburgh_url = "https://www.modulargrid.net/img/imagecache/131x413_6225.jpg"
 
+mum_m8_url = "https://www.modulargrid.net/img/imagecache/133x413_13679.jpg"
 
-
+spock_url = "https://www.modulargrid.net/img/imagecache/64x413_336.jpg"
 
 
 module_1 = Module(name="Mutant Bassdrum", description=mutant_bassdrum_description, stock="5",
                   buying_cost=150, selling_price=212, function="Drum", width=13, depth=35,
                   image_url=mutant_bassdrum_url, minus_12v=55, plus_12v=45, manufacturer=hexinverter)
 
-module_2 = Module(name="Mutant Clap", description="a short description", stock="3",
+module_2 = Module(name="Mutant Clap", description=mutant_clap_description, stock="6",
                   buying_cost=100, selling_price=180, function="Drum", width=13, depth=35,
-                  image_url=None, minus_12v=55, plus_12v=45, manufacturer=hexinverter)
+                  image_url=mutant_clap_url, minus_12v=55, plus_12v=45, manufacturer=hexinverter)
+
+module_3 = Module(name="Mutant Machine", description=mutant_machine_description, stock="1",
+                  buying_cost=100, selling_price=180, function="Drum", width=13, depth=35,
+                  image_url=mutant_machine_url, minus_12v=55, plus_12v=45, manufacturer=hexinverter)
+
+module_4 = Module(name="Mutant Hot Glue", description=mutant_hotglue_description, stock="1",
+                  buying_cost=100, selling_price=180, function="Drum", width=13, depth=35,
+                  image_url=mutant_hotglue_url, minus_12v=55, plus_12v=45, manufacturer=hexinverter)
+
+module_5 = Module(name="Twin Peaks Resonator", description=twin_peak_description, stock="1",
+                  buying_cost=100, selling_price=180, function="Drum", width=13, depth=35,
+                  image_url=twin_peak_res_url, minus_12v=55, plus_12v=45, manufacturer=klangbau)
+
+
 
 module_repository.save(module_1)
 module_repository.save(module_2)
+module_repository.save(module_3)
+module_repository.save(module_4)
+module_repository.save(module_5)
 
 
 pdb.set_trace()
--- sample_hold/descriptions.py
+++ sample_hold/descriptions.py
@@ -11,3 +11,139 @@ It's really two modules in one: a totally unique analogue bassdrum and a voltage
 - completely redesigned, hi-fi signal chain spares no expense by replacing cheap transistor amplifiers with low noise opamp equivalents
 - you can dial in super hot, modular level output levels (up to 20Vp-p!)"""
 
+mutant_clap_description = """====Primary Features====
+
+Just like the other Mutant Drums, the Clap is a highly versatile module. It is actually a voltage controlled LFSR noise oscillator as well as a powerful percussion synthesis engine!
+
+feature-rich, analogue handclap percussion synthesis reminiscent of the 909
+pitch control of noise generator means you can create everything from vanilla 909 claps to broken, chiptune-like alien sounds
+doubles as a voltage controlled, Commodore64-sounding noise VCO via the NOISE output and PITCH CV input
+SUSTAIN control lets you crank up the clap's sustain and use the module as an abstract synth voice with external inputs!
+external input allows you to synthesise claps from virtually any sound source you plug in
+built in internal reverb effect derived from clap tone
+reverb decay forms the decay of the clap sound, and is fully voltage controllable from short reverb "snaps" to long, whooshing tails!
+you can select between internal and external noise sources as the source for the reverb sound generation, opening up really interesting sounds
+DRIVE control lets you dial in anything from clean to extremely overdriven sounds
+bandpass filter cutoff control"""
+
+mutant_machine_description = """microcontroller-free analog percussion synthesis engine
+
+• unique Inverter Core oscillators form the MEMBRANE. Each analog oscillator has three waveforms to select from
+• capable of synthesizing everything from heavy-hitting bassdrums to classic 909-style snares and other complex timbres
+• modular design gives the Machine many auxiliary purposes, great for modular sound design of many varieties, and not just percussion
+• the SNAPPY section is comprised of a voltage controlled noise oscillator, for modelling the noisy part of drum timbres
+• both MEMBRANE and SNAPPY elements have an external input for replacing the built-in sound sources, opening up many avenues of possibility
+• 13 control voltage and audio inputs, for a fully modular drum experience
+• 7 audio and CV/gate outputs for maximum integration with other modules
+• dedicated outputs for each WAVE and NOISE oscillator mean you can use the Machine as a complex VCO in your system, when not synthesizing percussion
+
+waveform scanning feature generates complex timbres from analog waveforms
+• The MEMBRANE’s waveforms can be scanned through automatically by the wavescanner’s voltage controlled clock generator, or an external clock or VCO can be used
+• SCAN FREQ CV forms a unique form of timbre control, making the Machine act like a complex oscillator at its WAVES output
+• The ENABLE input allows you to gate the wavescanner on and off with a CV or gate signal
+
+Architecture of the Machine
+The Mutant Machine is a dynamic analog instrument capable of generating a wide palette of sounds, ranging from various forms of analog percussion to complex drones and oscillations. To achieve this, the Machine features two synthesis sections which are summed together at the final output: MEMBRANE and SNAPPY. Like the other Mutant Drums, the MEMBRANE and SNAPPY circuits began their mutation as classic analog percussion techniques and have been reimagined for 21st century modular synthesis.
+
+The MEMBRANE forms the main body of the sound by way of two analog VCOs, and the SNAPPY section further adds to the timbre by contributing noisy elements to the mix. A noisy CLICK which occurs at the beginning of the SNAPPY sound can have its volume adjusted independent of the main decaying SNAPPY texture.
+
+The waveforms which make up the MEMBRANE can be selected manually by button press, or the WAVESCANNER can be used to automatically scan through the available analog waveforms. By modulating the frequency through which waves are scanned, unique, complex sounds are created.
+
+Experimentation is encouraged by the many modulation inputs and outputs available to you. The Machine features 8 CV and gate inputs for modulation as well as two external audio inputs, for bringing other modules into the Machine’s core. There are many audio outputs for maximum versatility, allowing you to use the Machine to create drones and alien timbres for use elsewhere in the modular analog system."""
+
+mutant_hotglue_description = """4 channel mixer with effects send
+• 4 input channels, each with their own level and effects send control
+• Channel A mixer bypass switch and dedicated output for easy sidechain compression
+• Channel B is dual input, but shares level/send controls (for dual output modules, like the Mutant Hihats)
+• Channel C+D summed output
+• Main mixer output normalized to compressor input
+
+hi-fidelity analog compressor
+• Soft-knee compressor based on the same family of premium audio ICs used in some 500 Series studio gear
+• Blend control lets you adjust the dry/wet mix of the compressor, allowing for easy New York-style parallel compression
+• Dedicated threshold and compression ratio controls
+• Gain control dials in up to +40dB of compressor gain
+• Compressor sidechain input for pumping effects
+• LED indicator actively displays the amount of compressor gain reduction
+
+voltage controlled analog distortion effect
+• A classic distortion topology reborn for modular analog synthesizers and drum processing
+• In/Outputs are conveniently normalized to the module’s send and return jacks
+• Blend control lets you adjust the mix of distorted and clean input signal
+• Fast responding CV input allows for complex modulation patterns and even modulating with audio signals
+
+The Mutant Hot Glue is a powerful bus mixer for analog effects chain design. The included high quality analog compressor offers series or parallel compression, and a sidechain input, making it well suited for drum processing. The voltage controlled distortion is based on the batteryACID: IDOW Edition design, but improved for better signal fidelity and performance. It can be used in its default position, normalized to the effects send, or like every other part of the module, broken out and used elsewhere in your modular system.
+
+By default, the 4 channel mixer’s output conveniently feeds into the compressor’s input, giving access to easy New York style parallel compression by way of the compressor blend control. The dedicated output and mixer bypass switch for channel A allow you to easily use it as a sidechain source for the compressor. An effects send and return allow you to create complex effects routings within your modular system. With the number of excellent DSP and other effects modules on the market today, this makes it easy to combine modules to great effect, all within the eurorack modular domain."""
+
+twin_peak_description = """The core is the TwinPeak Filter principle, described in the TWP with Joystick module.
+CV1 and CV2 are normalised to Audio In and offers 1V/oct tuning.
+Direct tracking to both filters with Tr1 and Tr2. Both controllers are tuned for fast pings and ring sounds to 0.5V/oct.
+Open Resonance fully clockwise to set self resonance to get a sine wave. To play this "VCO'" it is well tunded at 1V/oct over a couple of octaves. Use XFM-2 to modulate F1 from F2. You get powerful FM sounds.
+LP/BP = BP both Filters are active, LP only F1is active. So BP is general TWP-mode!
+Res-mod: it is CV control for Resonance or Q-factor of both filters. Good for vocal spectra when modulated with a square wave."""
+
+math_description = """MATHS builds on the tradition set into motion in the 1960s when Don Buchla adapted circuits found within analog computers for musical purposes. Buchla's Algebraic Processor, Model 257 and 281 changed the way music synthesizers utilize control voltages. MATHS continues this great tradition - sculpting the voltages we use to sculpt sounds."""
+
+
+a_148_description = """Module A-148 (Dual S&H) has two identical sample & hold modules, designed to produce 'staircase' voltages. The signal present at the sample input is sampled at a rate set by the signal at the trigger input, and held at that voltage at the S&H output.
+
+The exact shape of the staircase depends on the sort of waveform at the sample input: NOISE or RANDOM signals produce random patterns; an LFO produces rising or falling staircase patterns.
+
+Two LEDs for each S&H indicate the voltage (positive or negative) of the sampled signal.
+
+For more detailed information please look at the English A-148 Dual S&H user's manual"""
+
+a_160_description = """Eurorack Module A-160 from Doepfer
+Clock Divider
+
+Module A-160 is a frequency divider for clock/trigger/gate signals, designed to be a source of lower frequencies, particularly for rhythm uses. The Trigger input will take clock signals from, eg., an LFO, MIDI sync, or the gate from a MIDI-CV interface.
+At the outputs, you have access to the sub-divided clock signals, from half the clock frequency down to 1/64. The low/high levels of the output signals are 0V and about +10V.
+The A-160 also has a reset input. Whenever a reset signal is sensed, all outputs are set to zero, until the reset voltage disappears.
+The Clock Divider can be used in combination with the A-161 Clock Sequencer to produce stepped sequences with a length of from one to eight events."""
+
+a_160_2_description = """Clock/Trigger Divider II
+
+Module A-160-2 is an enhanced version of the standard clock divider A-160. The module is a frequency divider for clock/trigger/gate signals, designed to be a source of lower frequencies, particularly for rhythm uses.
+
+The Clock input will take any digital signal from, eg., an LFO, MIDI sync, or the gate from a MIDI-CV interface. At the outputs, you have access to three sets of seven different sub-divided clock signals, from half the clock frequency down to 1/128. The low/high levels of the output signals are 0V and about +10V.
+
+The A-160-2 also has a reset input. Whenever a reset signal is sensed, all outputs are set to certain levels which depend upon the selected mode."""
+
+a_160_5_description = """Voltage Controlled Clock Multiplier / Ratcheting Controller
+
+Module A-160-5 is a voltage controlled clock multiplier. The incoming clock signal (socket Clock In) is multiplied by a factor that depends upon the control voltage on socket CV In (0...+5V) and the position of the Mode switch. The multiplied clock signal is available at the socket Clock Out. According to the position of the Mode switch different clock multiplying factors are assigned to the control voltage. With 0V CV no clock output is generated. With increasing CV integer factors (left position of the mode switch), power of two factors (middle position) or a mix of both (right position) are obtained. Nine LEDs are used to show the currently selected multiplying factor. In addition two LEDs are used to display the incoming and outgoing clock signal.
+
+The module can be used for all kind of clock multiplying applications. One important example is the generation of so-called ratcheting sequences. The band Tangerine Dream is famous for this kind of sequences. A normal sequencer generates only one gate signal per step. A ratcheting sequence may have also more than one gate pulses per step. This function can be obtained by using the A-160-5: one CV output of the sequencer is used to define the number of gate pulses per step. If the control of the step in question is fully CCW the generated CV is 0V and no gate signal is generated (mute of the step). When the control of the step in question is turned clockwise one, two or more gate pulses are generated depending upon the position of the mode switch and the voltage generated by the CV at this step.
+
+Note: Due to the nature of clock multiplying it takes a few input clock pulses until the clock output is stable. One has to average a few input clock pulses to generate the multiplied clock output signal. Even when the input clock frequency changes it will take a few cycles until the output clock signal is correct as the module cannot forsee the future of the clock input signal. The generated clock output signal is derived from the last few cycles of the clock input signal. Consequently the module should be driven only by a clock signal with constant or slowly changing frequency."""
+
+pittburgh_lpg_description= """Triple Mode Filter, VCA, Lopass Gate
+
+The Pittsburgh Modular Lopass Gate module is a new take on a classic. Our intention was not to clone but to reengineer and expand on the original idea. Our module includes an amazing new self-oscillating resonance circuit and a quieter vactrol based VCA circuit with true unity gain. Voltage control of the module is handled using an invertible, attenuated input and ping input. The ping input takes any gate or square wave and converts it into a very tight trigger used to ping the vactrol.
+
+In lowpass filter mode, frequency sweeps are warm and buttery and resonance is smooth and controlled until it gets near self oscillation. At this point the Pittsburgh Modular LPG module turns into a shouting beast. High resonance creates an edgier, shattered, more direct sound. It never loses control but is always teetering on the edge. Having both the smooth and snarling responses available creates a filter that is perfect for almost any situation.
+
+In VCA mode, we worked very hard to reduce the bleed associated with most lopass gate circuits without resorting to tricks like adding an a traditional VCA after the vactrol circuit. After considerable effort, we succeeded in creating a vactrol based VCA that is identical or better in performance to our 2164 based Dual VCA module.
+
+All the work we did on the filter and VCA come together with the lopass gate mode. The buttery warmth of the filter teams with the best attributes of the vactrol based VCA to create an impressive take on the original lopass gate.
+
+HISTORY AND THEORY BEHIND THE LOPASS GATE
+The lopass gate module is unique in the way that it simulates the characteristics of natural instruments. When used in lopass gate mode, louder sounds contain more harmonic content and quieter sounds contain less harmonic content. This is not the case when using a standard VCA such as the Pittsburgh Modular Dual VCA module. A standard VCA simply changes the loudness of the sound without changing the harmonic content. There is no reason that synthesizers need to simulate how acoustic instruments function but because of how it affects the harmonic content of sound, a lowpass gate sounds more organic or natural compared to a standard filter/vca chain.
+
+The circuitry behind the lopass gate has evolved over the years to include resonance and more complex modulation options but the ability to switch between voltage controlled amplifier, low pass filter, and a third mode which uses both the VCA and filter together remains at the core of the modules functionality. A lot of the characteristics associated with the lopass gate circuit come from the use of vactrols. A vactrol is an optoelectronic device consisting of an LED and light detector enclosed in a light tight package. The elements of the vactrol are optically coupled and electrically isolated from each other. When the LED within the vactrol turns on, the light detector reacts very quickly creating a very sharp attack, however, when the LED turns off, the light detector within the vactrol does not react as quickly and closes more slowly. That slow decay or ringing is the quality most associated with the lopass gate. Therefore, using a very short trigger to ping the vactrol will not result in a very short sound, instead the result is an organic percussive sound with an natural sounding decay.
+
+The lopass gate uses a Sallen-Key filter. The Sallen-Key filter is a 2 pole 12db filter with a very buttery, natural sound. In addition to the lopass gate, the Sallen-Key filter was used as the core of Korg's classic MS-10 and MS-20 filters. Although the Korg filters are remembered for their biting growl, that has more to do with the resonance then with the filter itself."""
+
+mum_m8_description = """Akai S950-based switched capacitor LPF
+
+‘MUM M8’ is an 8hp Eurorack low pass filter building on the filter design used in the classic Akai S950 12 bit sampler with the addition of resonance, voltage control, clipped saturation and a dedicated VCA.
+
+The low pass filter gives the recognisable super smooth vacuum like tone sucking sound which works magic on complex sources. The addition of clipped overdrive and intentionally aggressive self-oscillating resonance compliment the smooth filter core by juxtaposing it into more wasp like territory with acidic squeals and a more gritty sound.
+
+The filter core is a 6th Order Butterworth switched capacitor type design featuring an analog core but controlled by a high speed digital clock. This produces the characteristic smooth sounding filter but, as with the original S950, has a relatively high minimum cut off as to avoid ‘clock bleed’ into the filter output from the digital control. This minimum cut of point can be controlled via trimmers on the rear of the module."""
+
+spock_description = """Boolean Logic: The Spock module comprises two sets of CMOS based dual input logic gates with te following functions: AND, OR, XOR and NOT. The top and bottom half of
+the module are identical but can be combined together with patch cables to realise more complex logic functions.
+This module is ideally used with clocks, triggers and other source of square pulses. Interesting results are also achieved by feeding audiorate signals into the gate
+inputs and creating complex pulse based waveforms."""
